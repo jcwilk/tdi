b1fe46f6ad09824874ed14093943abaa0c2e0959
John Wilkinson <me@jcwilk.com>
2023-06-03 13:22:12 -0700

    need more prompt room

--- src/openai_api.ts
+++ src/openai_api.ts
@@ -108,7 +108,7 @@ export async function getChatCompletion(
       body: JSON.stringify({
         messages,
         model: "gpt-4",
-        max_tokens: 4000,
+        max_tokens: 2000,
         temperature,
         stream: true
       })
